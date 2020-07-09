14e231c57152f968c962781a3bbdbd672fda4250
mp-intelligo <mpechthalt@intelligo.com>
2020-07-09 08:08:53 +0300

    env variables

--- src/http/http.config.ts
+++ src/http/http.config.ts
@@ -1,8 +1,8 @@
 declare const location: Location;
 
-const { env: { NODE_ENV }} = process;
-
-export const apiBaseUrl = NODE_ENV === 'production' ? `${location.origin}/api` : `http://localhost:8080/api`;
+const envType = process.env.REACT_APP_ENV;
+console.log('envType:', envType)
+export const apiBaseUrl = envType === 'production' ? `${location.origin}/api` : `http://localhost:8080/api`;
 
 export enum HttpEndPoint {
     signin = '/auth/signin',
